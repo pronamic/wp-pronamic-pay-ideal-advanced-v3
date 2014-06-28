5b74f9b4fa816d5affc043af2a185fa6f72e3008
Remco Tolsma <info@remcotolsma.nl>
2014-06-28 11:24:27 +0200

    Init commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# WordPress Pay Gateway: iDEAL Advanced v3
+
+**iDEAL Advanced v3 driver for the WordPress payment processing library.**
+
+[![Build Status](https://travis-ci.org/wp-pay-gateways/ideal-advanced-v3.svg?branch=develop)](https://travis-ci.org/wp-pay-gateways/ideal-advanced-v3)
+[![Coverage Status](https://coveralls.io/repos/wp-pay-gateways/ideal-advanced-v3/badge.png?branch=develop)](https://coveralls.io/r/wp-pay-gateways/ideal-advanced-v3?branch=develop)
+
+## Providers
+
+*	ING
+*	Rabobank
---
+++ tests/ErrorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Title: iDEAL Advanced error test
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_IDealAdvancedV3_ErrorTest extends PHPUnit_Framework_TestCase {
+	public function testToStringError() {
+		$error = new Pronamic_WP_Pay_Gateways_IDealAdvancedV3_Error();
+		$error->set_code( '1' );
+		$error->set_message( 'Error' );
+
+		$string = (string) $error;
+
+		$expected = '1 Error';
+
+		$this->assertEquals( $expected, $string );
+	}
+}
