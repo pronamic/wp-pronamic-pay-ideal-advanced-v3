e0727e10060efb41641d03fc2057bdd4baa35593
Remco Tolsma <info@remcotolsma.nl>
2014-07-03 15:02:07 +0200

    Also test the get_suggested_action() function, value should be null.

--- tests/XML/ErrorParserTest.php
+++ tests/XML/ErrorParserTest.php
@@ -22,6 +22,7 @@ class Pronamic_WP_Pay_Gateways_IDealAdvancedV3_XML_ErrorParserTest extends PHPUn
 		$this->assertSame( 'SO1100', $error->get_code() );
 		$this->assertSame( 'Issuer not available', $error->get_message() );
 		$this->assertSame( 'System generating error: Rabobank', $error->get_detail() );
+		$this->assertSame( null, $error->get_suggested_action() );
 		$this->assertSame( 'De geselecteerde iDEAL bank is momenteel niet beschikbaar i.v.m. onderhoud tot naar verwachting 31-12-2010 03:30. Probeer het later nogmaals of betaal op een andere manier.', $error->get_consumer_message() );
 	}
 }
