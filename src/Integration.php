2756f17a2dee3053f6f56c2eb9d9a51acff7ce63
Remco Tolsma <info@remcotolsma.nl>
2018-01-10 15:57:12 +0100

    Added a not abstract iDEAL Advanced v3 integration class.

---
+++ src/Integration.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Title: iDEAL Advanced v3 integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2018
+ * Company: Pronamic
+ *
+ * @author Remco Tolsma
+ * @version unreleased
+ * @since unreleased
+ */
+class Pronamic_WP_Pay_Gateways_IDealAdvancedV3_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvancedV3_AbstractIntegration {
+
+}
