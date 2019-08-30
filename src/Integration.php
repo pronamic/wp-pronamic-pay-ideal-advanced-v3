95c54600d81509895cae57950301e2631133f223
Remco Tolsma <info@remcotolsma.nl>
2019-08-30 16:09:42 +0200

    2.0.4

--- src/Integration.php
+++ src/Integration.php
@@ -11,7 +11,7 @@ use Pronamic\WordPress\Pay\Gateways\IDeal\AbstractIntegration;
  * Company: Pronamic
  *
  * @author  Remco Tolsma
- * @version 2.0.0
+ * @version 2.0.4
  * @since   2.0.0
  */
 class Integration extends AbstractIntegration {
